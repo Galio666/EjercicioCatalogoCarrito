f16a61a7848206a51475a01000f522725ad26c17
Galio666 <ascencio847@gmail.com>
2019-10-28 10:39:29 -0600

    Metodo agregar

--- app/src/main/java/com/example/ejerciciocatalogocarrito/MainActivity.java
+++ app/src/main/java/com/example/ejerciciocatalogocarrito/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ejerciciocatalogocarrito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ejerciciocatalogocarrito.Servicio.ProductoServicio;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
--- app/src/main/java/com/example/ejerciciocatalogocarrito/Servicio/ProductoServicio.java
+++ app/src/main/java/com/example/ejerciciocatalogocarrito/Servicio/ProductoServicio.java
@@ -1,10 +1,24 @@
 package com.example.ejerciciocatalogocarrito.Servicio;
 
 
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ejerciciocatalogocarrito.Modelo.Producto;
+import com.example.ejerciciocatalogocarrito.R;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 
-public class ProductoServicio {
+public class ProductoServicio extends AppCompatActivity {
+
+    TextView txtNombre;
+    TextView txtDescripcion;
+    TextView txtPrecio;
+    TextView marca;
 
   public ProductoServicio(){
         // Write a message to the database
@@ -12,13 +26,15 @@ public class ProductoServicio {
         DatabaseReference myRef = database.getReference("message");
     }
 
-    public void  agregarProducto(){
+    public void  agregarProducto(Producto producto){
+
+
 
     }
 
-    public void mostrarProducto(){
 
-    }
+    public void mostrarProducto(DataSnapshot dataSnapshot){
+  }
 
     public void editarPoducto(){
 
